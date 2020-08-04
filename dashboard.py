c122f47e2f8ef5025aba652c3ffc925a3edaaffd
steven112163 <steven112163@gmail.com>
2020-08-04 23:18:53 +0800

    Add widgets in django dashboard

--- dashboard.py
+++ dashboard.py
@@ -7,9 +7,38 @@ class CustomIndexDashboard(Dashboard):
     columns = 3
 
     def init_with_context(self, context):
+        self.children.append(modules.LinkList(
+            _('Support'),
+            children=[
+                {
+                    'title': _('Django documentation'),
+                    'url': 'http://docs.djangoproject.com/',
+                    'external': True,
+                },
+                {
+                    'title': _('Django "django-users" mailing list'),
+                    'url': 'http://groups.google.com/group/django-users',
+                    'external': True,
+                },
+                {
+                    'title': _('Django irc channel'),
+                    'url': 'irc://irc.freenode.net/django',
+                    'external': True,
+                },
+            ],
+            column=0,
+            order=0
+        ))
+        self.children.append(modules.Feed(
+            _('Latest Django News'),
+            feed_url='http://www.djangoproject.com/rss/weblog/',
+            limit=10,
+            column=1,
+            order=0
+        ))
         self.children.append(modules.RecentActions(
             _('Recent Actions'),
             10,
-            column=0,
+            column=2,
             order=0
         ))

1f57dbfd240d71bc4f82a502bfa78cde17cf155f
steven112163 <steven112163@gmail.com>
2020-08-05 17:43:52 +0800

    Setup item detail page

--- NuxtFrontend/pages/wiki/items/_id/index.vue
+++ NuxtFrontend/pages/wiki/items/_id/index.vue
@@ -1,10 +1,190 @@
 <template>
-
+    <b-container class="my-5">
+        <b-row>
+            <b-col cols="4">
+                <h1>{{ item.name }}</h1>
+            </b-col>
+            <b-col class="text-right" cols="8">
+                <p class="text-muted">Last Modified Date: {{ item.update }}</p>
+            </b-col>
+        </b-row>
+        <hr/>
+        <b-row>
+            <b-col class="text-center" cols="4">
+                <b-img fluid rounded :src="item.image" alt=""></b-img>
+            </b-col>
+            <b-col cols="8">
+                <table class="table table-hover">
+                    <tbody>
+                    <tr v-if="material">
+                        <th scope="row">Material</th>
+                        <td>{{ material.name }}</td>
+                    </tr>
+                    <tr v-if="type">
+                        <th scope="row">Type</th>
+                        <td>{{ type.name }}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Health Points</th>
+                        <td>
+                            <b-icon icon="battery-full" variant="primary"></b-icon>
+                            <span> {{ item.durability }}</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Stackable</th>
+                        <td>
+                            <b-icon v-if="item.stackable" icon="check-square-fill" variant="success"></b-icon>
+                            <b-icon v-else icon="x-square-fill" variant="danger"></b-icon>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Renewable</th>
+                        <td>
+                            <b-icon v-if="item.renewable" icon="check-square-fill" variant="success"></b-icon>
+                            <b-icon v-else icon="x-square-fill" variant="danger"></b-icon>
+                        </td>
+                    </tr>
+                    <tr v-if="attack">
+                        <th scope="row">Attack Damage</th>
+                        <td>
+                            <template v-for="i in attack">
+                                <b-icon icon="lightning-fill" variant="warning"></b-icon>
+                            </template>
+                            <template v-if="half_attack">
+                                <b-icon icon="lightning" variant="warning"></b-icon>
+                            </template>
+                            <span> ({{ item.attack_damage }})</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Attack Speed</th>
+                        <td>{{ item.attack_speed }}</td>
+                    </tr>
+                    <tr v-if="defense">
+                        <th scope="row">Defense Points</th>
+                        <td>
+                            <template v-for="i in defense">
+                                <b-icon icon="shield-fill" variant="secondary"></b-icon>
+                            </template>
+                            <template v-if="half_defense">
+                                <b-icon icon="shield-shaded" variant="secondary"></b-icon>
+                            </template>
+                            <span> ({{ item.defense_points }})</span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
+    import {
+        BIcon,
+        BIconLightning,
+        BIconLightningFill,
+        BIconBatteryFull,
+        BIconCheckSquareFill,
+        BIconXSquareFill,
+        BIconShieldFill,
+        BIconShieldShaded,
+    } from "bootstrap-vue";
+
     export default {
-        name: "index"
+        name: "itemIndex",
+        layout: "wiki",
+        head() {
+            return {
+                title: "Wiki | Item Detail"
+            };
+        },
+        async asyncData({$axios, params}) {
+            try {
+                let item = await $axios.$get(`/items/${params.id}`);
+
+                // Parse date
+                let idxDot = item.update.indexOf(".");
+                let idxT = item.update.indexOf("T");
+                item.update = item.update.substring(0, idxT) + " " + item.update.substring(idxT + 1, idxDot);
+
+                // Get number of attack damage symbols
+                let half_attack = false;
+                let attack = false;
+                if (item.attack_damage !== null) {
+                    half_attack = (item.attack_damage % 2) != 0 ? true : false;
+                    attack = half_attack ? ((item.attack_damage - 1) / 2) : (item.attack_damage / 2);
+                    if (item.attack_damage == 1)
+                        attack = 1;
+                    else if (item.attack_damage == 2)
+                        attack = 2;
+                }
+
+                // Get number of defense symbols
+                let half_defense = false;
+                let defense = false;
+                if (item.defense_points !== null) {
+                    half_defense = (item.defense_points % 2) != 0 ? true : false;
+                    defense = half_defense ? ((item.defense_points - 1) / 2) : (item.defense_points / 2);
+                    if (item.defense_points == 1)
+                        defense = 1;
+                    else if (item.defense_points == 2)
+                        defense = 2;
+                }
+
+                // Get material
+                let material = false;
+                if (item.material !== null)
+                    material = await $axios.$get(`/itemMaterials/${item.material}`);
+
+                // Get type
+                let type = false;
+                if (item.type !== null)
+                    type = await $axios.$get(`/itemTypes/${item.type}`);
+
+                return {
+                    item: item,
+                    material: material,
+                    type: type,
+                    half_attack: half_attack,
+                    attack: attack,
+                    half_defense: half_defense,
+                    defense: defense
+                };
+            } catch (e) {
+                return {
+                    item: [],
+                    material: [],
+                    type: [],
+                    half_attack: [],
+                    attack: [],
+                    half_defense: [],
+                    defense: []
+                };
+            }
+        },
+        data() {
+            return {
+                item: [],
+                material: [],
+                type: [],
+                half_attack: [],
+                attack: [],
+                half_defense: [],
+                defense: []
+            };
+        },
+        components: {
+            BIcon,
+            BIconLightning,
+            BIconLightningFill,
+            BIconBatteryFull,
+            BIconCheckSquareFill,
+            BIconXSquareFill,
+            BIconShieldFill,
+            BIconShieldShaded
+        }
     }
 </script>
 
